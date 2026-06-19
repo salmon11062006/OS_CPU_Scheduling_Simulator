e6b5be504884b4fc50a3231fb3b41c227a1f0161
Christian Salomo <csalomo7@gmail.com>
2026-06-19 21:25:50 +0700

    RR done #2

--- RR.py
+++ RR.py
@@ -1,18 +1,16 @@
-from collections import deque
-
 from Helper import Task
 
 def round_robin(tasks, quantum=2):
     tasks.sort(key=lambda x: x.arrival_time)
     current_time = 0
-    queue = deque()
+    queue = []
     not_arrived = list(tasks)
 
     while not_arrived and not_arrived[0].arrival_time <= current_time:
         queue.append(not_arrived.pop(0))
 
     while queue:
-        task = queue.popleft()
+        task = queue.pop(0)
         
         if current_time < task.arrival_time:
             current_time = task.arrival_time
